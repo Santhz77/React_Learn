896fbdfd649269ccd6f8bcf935d5dea766eddef4
Santhz77 <santhoshnayak0903gmail.com>
2021-01-13 07:02:45 +0100

    added simple components, data props

--- src/App.js
+++ src/App.js
@@ -27,10 +27,29 @@ import Table from "./Table"
 
 class App extends Component {
   render() {
+
+    const characters = [
+      {
+          name: 'Charlie',
+          job: 'Janitor',
+      },
+      {
+          name: 'Mac',
+          job: 'Bouncer',
+      },
+      {
+          name: 'Dee',
+          job: 'Aspring actress',
+      },
+      {
+          name: 'Dennis',
+          job: 'Bartender',
+      },
+  ]
     return (
       <div className="App">
-        <h1>Hello, React!</h1>
-        <Table/>
+        <h1>Jobs CURD</h1>
+        <Table data={characters}/>
       </div>
     )
   }
--- src/Table.js
+++ src/Table.js
@@ -1,37 +1,40 @@
 import React, { Component } from 'react'
+const TableHeader = () => {
+    return (<thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+        </tr>
+    </thead>)
+}
+
+const TableBody = (props) => {
+    const rows = props.data.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+
+            </tr>
+        )
+
+    })
+    return <tbody>{rows}</tbody>
+}
 
 class Table extends Component {
     render() {
+        const { data } = this.props
+
 
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Charlie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div className="container">
+                <table >
+                    <TableHeader />
+                    <TableBody data={data} />
+                </table>
+            </div>
         )
     }
 }
-
 export default Table
